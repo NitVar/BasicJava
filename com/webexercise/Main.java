c6cb1beaf8cf92d4f6959aef17108fac8a900a2c
NitVar <126921817+NitVar@users.noreply.github.com>
2023-09-21 16:55:20 -0400

    Merge remote-tracking branch 'origin/main'

--- com/webexercise/Main.java
+++ com/webexercise/Main.java
@@ -2,11 +2,7 @@ package com.webexercise;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 10;
-        i = i++;
-        i = ++i;
-        i = i++ + ++i;
-        System.out.println(i);
+
         System.out.println("Hello and welcome!");
 
 
