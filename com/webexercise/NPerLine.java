9eb05348789b9d96364eb42464a8e7527157768a
NitVar <126921817+NitVar@users.noreply.github.com>
2023-09-20 21:14:38 -0400

    NPerLine

--- com/webexercise/NPerLine.java
+++ com/webexercise/NPerLine.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /*Write a program NPerLine.java that takes an integer command-line argument n and prints the integers
 from 10 to 99 with n integers per line.
-   */
+*/
 public class NPerLine {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
