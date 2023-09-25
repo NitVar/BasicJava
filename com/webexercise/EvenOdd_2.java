2128fd3bb8f17505dac8c58185ddc0929c15f159
NitVar <126921817+NitVar@users.noreply.github.com>
2023-09-24 22:28:42 -0400

    Merge remote-tracking branch 'origin/main'

--- com/webexercise/EvenOdd_2.java
+++ com/webexercise/EvenOdd_2.java
@@ -1,2 +1,17 @@
-package com.webexercise;public class EvenOdd_2 {
+package com.webexercise;
+
+import java.util.Scanner;
+
+/*Write a program to check if a given number is odd or even.*/
+public class EvenOdd_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        if (num%2 == 0)
+            System.out.println("Even");
+
+        else
+            System.out.println("Odd");
+    }
 }
